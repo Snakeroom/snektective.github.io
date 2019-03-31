4f8eef033c885620392e84c4fec59181ab38723d
lepon01 <katie@discordmail.com>
2019-03-31 17:16:59 +0100

    add github links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,59 +13,119 @@ const categories = [
     content: 'sneknet icon courtesy of'
   }
 ]
-
 const users = [
   {
     reddit: '__-_------___---',
+    github: '9hp',
     category: 'site'
   }, {
     reddit: 'offbeatwitch',
+    github: 'hedgehog1029',
     category: 'site'
   }, {
     reddit: '7coil',
+    github: '7coil',
     category: 'site'
   }, {
     reddit: 'taxborn',
+    github: 'taxborn',
     category: 'contributors'
   }, {
     reddit: 'seshpenguin',
+    github: 'Seshpenguin',
     category: 'contributors'
   }, {
     reddit: 'thesbros',
+    github: 'odensc',
     category: 'contributors'
   },{
     reddit: 'goz3rr',
+    github: 'goz3rr',
     category: 'contributors'
   },{
     reddit: 'cubity_first',
+    github: 'CubityCube',
     category: 'contributors'
   },{
     reddit: 'Hexbugman213',
+    github: null,
     category: 'logo'
   }
 ]
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.onPress = this.onPress.bind(this);
+    this.onRelease = this.onRelease.bind(this);
+
+    this.state = {
+      mode: 'reddit'
+    }
+  }
+  onPress(event) {
+    if (event.keyCode === 17) {
+      this.setState({
+        mode: 'github'
+      })
+    }
+  }
+  onRelease(event) {
+    if (event.keyCode === 17) {
+      this.setState({
+        mode: 'reddit'
+      })
+    }
+  }
+  componentDidMount() {
+    document.addEventListener('keydown', this.onPress, false);
+    document.addEventListener('keyup', this.onRelease, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.onPress, false);
+    document.removeEventListener('keyup', this.onRelease, false);
+  }
   render() {
+    const { mode } = this.state;
     return (
       <footer className={styles.footer}>
-        {categories.map(category => (
-          <p key={category.id}>
-            {category.content}<br />
-            <div className={styles.links}>
-              {
-                users
-                  .filter(user => user.category === category.id)
-                  .map(user => <a key={user.reddit} href={`https://reddit.com/u/${user.reddit}`}>{`/u/${user.reddit}`}</a>)
-              }
-            </div>
-          </p>
-        ))}
-        <p>
-          <small>
-            &lt;3 reactjs
-          </small>
-        </p>
+        {
+          categories
+            .filter(category => // Filter out categories that have no users in it
+              users
+                .filter(user => user.category === category.id)
+                .filter(user => user[mode])
+                .length > 0
+            )
+            .map(category => (
+              <p key={category.id}>
+                {category.content}<br />
+                <span className={styles.links}>
+                  {
+                    users
+                      .filter(user => user.category === category.id)
+                      .filter(user => user[mode])
+                      .map(user => {
+                        const username = user[mode]
+
+                        if (mode === 'reddit') return (
+                          <a key={username} href={`https://reddit.com/u/${username}`}>{`/u/${username}`}</a>
+                        )
+                      
+                        if (mode === 'github') return (
+                          <a key={username} href={`https://github.com/${username}`}>{username}</a>
+                        )
+
+                        // Return the username if unknown mode
+                        return (
+                          <span>{username}</span>
+                        )
+                      })
+                  }
+                </span>
+              </p>
+            ))
+        }
       </footer>
     )
   }
