e002d55af24553de716150f6cb3ea1b71b4a3559
Brandon Dyer <brandondyer64@gmail.com>
2019-04-01 20:23:44 -0500

    New API

--- src/components/GifList/getData.js
+++ src/components/GifList/getData.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
 export function getData() {
-  return axios
-    .get(`https://api.myjson.com/bins/obxx6.json`)
-    .then(res => res.data)
+  return axios.get(`https://snake.egg.party/v1/targets`).then(res => res.data)
 }
