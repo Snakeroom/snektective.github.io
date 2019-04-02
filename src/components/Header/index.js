73612882187b2f217e2a6981b5425ec68bfdb603
Brandon Dyer <brandondyer64@gmail.com>
2019-04-01 21:56:49 -0500

    Install buttons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,11 +18,38 @@ class Header extends Component {
           <h1 className={styles.name}>Sneknet</h1>
         </Link>
         <div className={styles.links}>
-          <a href="https://github.com/Snektective">github</a>
-          <a href="https://discord.gg/yq9QV2A">snakeroom</a>
-          <a href="https://discord.gg/f9xaeJG">april knights</a>
-          <a href="https://discord.gg/sfe">sfe</a>
-          <a href="https://discord.gg/3rcujhh">sequence narrators</a>
+          <a href="https://github.com/Snektective" target="_blank">
+            github
+          </a>
+          <a href="https://discord.gg/yq9QV2A" target="_blank">
+            snakeroom
+          </a>
+          <a href="https://discord.gg/f9xaeJG" target="_blank">
+            april knights
+          </a>
+          <a href="https://discord.gg/sfe" target="_blank">
+            sfe
+          </a>
+          <a href="https://discord.gg/3rcujhh" target="_blank">
+            sequence narrators
+          </a>
+        </div>
+        <div className={styles.installButtons}>
+          <h2>Install Sneknet</h2>
+          <span className={styles.buttons}>
+            <a
+              href="https://addons.mozilla.org/en-US/firefox/addon/snek/"
+              target="_blank"
+            >
+              Firefox
+            </a>
+            <a
+              href="https://chrome.google.com/webstore/detail/snek/cdajbijppfenmcmgnhekghefilinkfnm"
+              target="_blank"
+            >
+              Chrome
+            </a>
+          </span>
         </div>
       </header>
     )
